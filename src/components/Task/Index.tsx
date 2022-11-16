09ef976f26d086ea3b41f7a0e53dcdda5fc2fce4
Rafael Feitosa <rafaefeitosa@gmail.com>
2022-11-16 16:03:47 -0300

    Commit: Adicionado break-words.

--- src/components/Task/Index.tsx
+++ src/components/Task/Index.tsx
@@ -28,7 +28,7 @@ export function Task({ task, onDelete, onComplete }: Props) {
 				)}
 			</button>
 
-			<p className={task.isCompleted ? "text-sm text-gray-300 line-through mr-auto" :"text-sm text-gray-100 mr-auto"}>{task.title}</p>
+			<p className={task.isCompleted ? "text-sm text-gray-300 line-through mr-auto overflow-hidden break-words" :"text-sm text-gray-100 mr-auto overflow-hidden break-words"}>{task.title}</p>
 
 			<button
 				className="text-gray-300 hover:text-red-500 transition-all"
